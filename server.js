7f941b89874e7b778b48c61d26e2c6792db607e2
Esteban Douillet <contact@lowan.fr>
2024-10-05 19:57:41 +0200

    🩹 Transformation en module ES

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-const express = require('express');
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Créer une application express
 const app = express();
 const PORT = 3000;
-const bodyParser = require('body-parser');
-const path = require("path");
 
 // Middleware
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Recréer __dirname dans un module ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
 
 // Route principale
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/html/homepage.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/html/homepage.html"));
 });
 
 // Lance le serveur sur le port 3000
