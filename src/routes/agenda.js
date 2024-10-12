40001c25148a411ec253f9a1eb7c0626ae40604b
Esteban Douillet <contact@lowan.fr>
2024-10-12 14:56:26 +0200

    🚩 Mise en place de la fonctionnalité avec une page de test temporairement (/agenda_test)

--- src/routes/agenda.js
+++ src/routes/agenda.js
@@ -1,11 +1,12 @@
 import Routeur from "../structures/Routeur.js";
+import moment from "moment";
 
 /**
  * Les routes liées à la page de l'agenda
  */
 class AgendaRouteur extends Routeur {
-  constructor() {
-    super();
+  constructor(server) {
+    super(server);
   }
 
   /**
@@ -15,8 +16,66 @@ class AgendaRouteur extends Routeur {
     this.router.get("/agenda", (req, res) => {
       res.render("agenda");
     });
+
+    this.router.get("/agenda_test", (req, res) => {
+      const view = req.query.view || "month"; // Par défaut, on affiche la vue du mois
+      moment.locale("fr");
+      const currentDate = moment(req.query.date) || moment();
+
+      let prevDate, nextDate, weekStart, weekEnd;
+
+      if (view === "day") {
+        prevDate = currentDate.clone().subtract(1, "day").format("YYYY-MM-DD");
+        nextDate = currentDate.clone().add(1, "day").format("YYYY-MM-DD");
+      } else if (view === "week") {
+        weekStart = currentDate.clone().startOf("week");
+        weekEnd = currentDate.clone().endOf("week");
+        prevDate = weekStart.clone().subtract(1, "week").format("YYYY-MM-DD");
+        nextDate = weekStart.clone().add(1, "week").format("YYYY-MM-DD");
+      } else if (view === "month") {
+        prevDate = currentDate
+          .clone()
+          .subtract(1, "month")
+          .format("YYYY-MM-DD");
+        nextDate = currentDate.clone().add(1, "month").format("YYYY-MM-DD");
+      }
+
+      // Appelle la fonction qui retourne les événements pour la date spécifiée
+      const events = getEventsForDate(this.server, currentDate);
+
+      res.render("agenda_test", {
+        view,
+        currentDate,
+        prevDate,
+        nextDate,
+        weekStart,
+        weekEnd,
+        events,
+        moment
+      });
+    });
   }
 }
 
-const agendaRouteur = new AgendaRouteur();
-export default agendaRouteur.router;
+/**
+ * Retourne les événements
+ * @param server {KiAvenir}
+ * @param currentDate {moment}
+ * @returns {Object[]}
+ */
+function getEventsForDate(server, currentDate) {
+  return server.database.tables
+    .get("events")
+    .getAll()
+    .filter((event) => event.startDate.getMonth() === currentDate.month())
+    .map((event) => {
+      return {
+        title: event.name,
+        details: event.description,
+        start: moment(event.startDate),
+        end: moment(event.endDate)
+      };
+    });
+}
+
+export default AgendaRouteur;
