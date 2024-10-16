c972cb168549e05160ef99d4882c224e31d65a2b
Esteban Douillet <contact@lowan.fr>
2024-10-16 21:05:05 +0200

    🩹 Corrige la page d'accueil

---
+++ src/public/js/account/logout.js
@@ -0,0 +1,6 @@
+function sendLogout() {
+  document.getElementById("logoutForm").submit();
+}
+
+// Attacher l'événement au bouton menu
+document.getElementById("logout_link").addEventListener("click", sendLogout);
