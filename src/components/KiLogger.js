80ec8fc04426ff3dd1c4109b4f746cd5d4a27435
Esteban Douillet <contact@lowan.fr>
2024-10-27 17:54:50 +0100

    ⚰️ Suppression d'éléments inutiles

--- src/components/KiLogger.js
+++ src/components/KiLogger.js
@@ -127,16 +127,12 @@ export default class KiLogger {
 /**
  * Parse la date
  * @param date {Date} La date
- * @returns {{month: number, hour: (string|number), year: number, minutes: (string|number), day: (string|number), secondes: (string|number)}}
+ * @returns {{year: number, month: number, day: string}}
  */
 function parseDate(date = new Date()) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
 
-  const hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
-  const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
-  const secondes = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
-
-  return { year, month, day, hour, minutes, secondes };
+  return { year, month, day };
 }
