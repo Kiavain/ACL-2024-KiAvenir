03617c590c7cd8506292d98770ce9e24f14c1283
Hikikomori041 <nicolas.blachere@gmail.com>
2024-11-21 15:34:53 +0100

    chargement des avatars en fonction de l'id du user

--- src/public/js/account/loadUserIcon.js
+++ src/public/js/account/loadUserIcon.js
@@ -1,12 +1,15 @@
-const iconPath = "/img/user_icon/1.jpg"; //todo: recuperer l'id utilisateur (par exemple dans un datatype chéplukoi)
+const userIcon = document.getElementById('userIcon');
+const userId = userIcon.dataset.userId;
+const iconPath = "/img/user_icon/" + userId +".jpg";
+const defaultPath = "/img/user_icon/account.jpg";
 
 fetch(iconPath, { method: 'HEAD' })
   .then((response) => {
-    if (response.ok) {
-        // console.log(`${iconPath} existe.`);
-        document.getElementById("userIcon").src = iconPath;
+    if (response.ok && response.headers.get('Content-Type').startsWith('image/')) {
+        // console.log(`${iconPath} existe, on la récupère.`);
+        userIcon.src = iconPath;
     } else {
-        console.log(`${iconPath} n'existe pas.`);
+        // console.log(`${iconPath} n'existe pas, on utilise l'image par défaut.`);
     }
   })
   .catch(() => {
