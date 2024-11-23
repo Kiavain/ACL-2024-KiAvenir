f039b562017d056e3b995244ca495464ef8bf327
khoualed2u <rayan.khoualed3@etu.univ-lorraine.fr>
2024-11-23 20:54:20 +0100

    Menu déroulant fonctionnel. Manque à rajouter la suppresion/modification de l'agenda dans le menu

--- src/public/js/agenda/agendaMenu.js
+++ src/public/js/agenda/agendaMenu.js
@@ -1,25 +1,5 @@
-// Fonction pour afficher/masquer le menu
-function toggleMenu() {
-  const dropdownMenu = document.getElementById("dropdown-menu");
-  const isVisible = dropdownMenu.style.display === "block";
-  dropdownMenu.style.display = isVisible ? "none" : "block";
-}
 const shareOrExport = document.getElementById("shareOrExport");
-// Attacher l'événement au bouton menu
-document.getElementById("menu-button").addEventListener("click", toggleMenu);
 
-// Fermer le menu lorsque l'utilisateur clique en dehors
-document.addEventListener("click", function (event) {
-  const dropdownMenu = document.getElementById("dropdown-menu");
-  const menuButton = document.getElementById("menu-button");
-
-  if (!dropdownMenu.contains(event.target) && !menuButton.contains(event.target)) {
-    dropdownMenu.style.display = "none";
-  }
-  if (!shareOrExport.contains(event.target) && !dropdownMenu.contains(event.target)) {
-    shareOrExport.style.display = "none";
-  }
-});
 const agendaItems = document.getElementById("agenda-list").children;
 for (let i = 0; i < agendaItems.length; i++) {
   const agendaItem = agendaItems[i];
@@ -30,9 +10,16 @@ for (let i = 0; i < agendaItems.length; i++) {
     toggleMenuShareOrExport(agendaItemValue, agendaItemName);
   });
 }
+document.addEventListener("DOMContentLoaded", () => {
+  // Fermer la modale si on clique ailleurs (et enlève l'écouteur)
+  window.addEventListener("click", function handleClickOutside(event) {
+    if (event.target !== shareOrExport) {
+      shareOrExport.style.display = "none";
+    }
+  });
+});
 function toggleMenuShareOrExport(val, name) {
   shareOrExport.style.display = "flex";
-  toggleMenu();
   shareOrExport.value = val;
   document.getElementById("agendaName").textContent = name;
 }
