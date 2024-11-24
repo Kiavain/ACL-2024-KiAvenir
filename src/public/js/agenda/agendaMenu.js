79e43268c1a4ed7d4e5e2fba2a32415795bb75e1
khoualed2u <rayan.khoualed3@etu.univ-lorraine.fr>
2024-11-24 17:04:17 +0100

    Amélioration du css

--- src/public/js/agenda/agendaMenu.js
+++ src/public/js/agenda/agendaMenu.js
@@ -9,7 +9,7 @@ for (let i = 0; i < agendaItems.length; i++) {
   const agendaItemColor = agendaItem.querySelector("a").dataset.agendaColor;
   const agendaItemDescription = agendaItem.querySelector("a").dataset.agendaDescription;
   agendaShareButton.addEventListener("click", () => {
-    toggleMenuShareOrExport(agendaItemValue, agendaItemName, agendaItemColor, agendaItemDescription);
+    toggleMenuShareOrExport(agendaItemValue, agendaItemName, agendaItemColor, agendaItemDescription, agendaShareButton);
   });
 }
 document.addEventListener("DOMContentLoaded", () => {
@@ -20,11 +20,16 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
-function toggleMenuShareOrExport(val, name, color, description) {
-  shareOrExport.style.display = "flex";
+function toggleMenuShareOrExport(val, name, color, description, button) {
+  shareOrExport.style.display = "grid";
   shareOrExport.value = val;
   document.getElementById("new-agenda-name").value = name;
   document.getElementById("new-agenda-color").value = color;
   document.getElementById("new-agenda-description").textContent = description;
   document.getElementById("agendaName").textContent = name;
+  // Positionnement dynamique
+  const buttonRect = button.getBoundingClientRect();
+  const containerRect = document.getElementById("calendar-container").getBoundingClientRect();
+  shareOrExport.style.position = "absolute";
+  shareOrExport.style.top = `${buttonRect.bottom - containerRect.top + window.scrollY + 5}px`;
 }
