f4f8161d1a0cf43d43392a77234c7e42d13454c1
Esteban Douillet <contact@lowan.fr>
2024-12-04 16:54:30 +0100

    🎨 Amélioration et documentation du Mailer

--- src/components/Mailer.js
+++ src/components/Mailer.js
@@ -4,37 +4,57 @@ import path from "path";
 import { getDirname } from "../utils/index.js";
 import dotenv from "dotenv";
 
+/**
+ * Typage de l'utilisateur
+ * @typedef {import("../entities/structures/User.js").default} User
+ */
+
+const MAIL_ENV = dotenv.config({ path: "./mail.env" }).parsed;
+
+/**
+ * Représente le mailer
+ */
 export default class Mailer {
+  /**
+   * Construit le mailer
+   * @param server {KiAvenir} Le serveur
+   */
   constructor(server) {
-    const mailEnv = dotenv.config({ path: "./mail.env" }).parsed;
     this.server = server;
     this.transport = nodeMailer.createTransport({
       service: "gmail",
       auth: {
-        user: mailEnv.EMAIL,
-        pass: mailEnv.EMAIL_PASSWORD
+        user: MAIL_ENV.EMAIL,
+        pass: MAIL_ENV.EMAIL_PASSWORD
       }
     });
   }
 
-  sendResetPasswordEmail(user, token) {
-    let htmlFile = path.join(getDirname(import.meta.url), "../mail/reset_password.html");
-    let html = fs.readFileSync(htmlFile, "utf8");
+  /**
+   * Envoie un mail pour réinitialiser le mot de passe
+   * @param user {User} L'utilisateur
+   * @param token {String} Le token de réinitialisation
+   * @returns {*}
+   */
+  async sendResetPasswordEmail(user, token) {
+    // Récupère le chemin du fichier de template
+    const htmlFile = path.join(getDirname(import.meta.url), "../mail/reset_password.html");
 
-    html = html
+    // Récupère le contenu du fichier de template en remplaçant les variables
+    const html = fs
+      .readFileSync(htmlFile, "utf8")
       .replaceAll("{{token}}", token)
       .replaceAll("{{email}}", user.email)
       .replaceAll("{{address}}", this.server.ADDRESS)
       .replaceAll("{{port}}", this.server.PORT)
       .replaceAll("{{username}}", user.username);
 
-    const mailOptions = {
-      from: `KiAvenir <${process.env.EMAIL}>`,
+    // Envoie le mail
+    await this.transport.sendMail({
+      from: `KiAvenir <${MAIL_ENV.EMAIL}>`,
       to: user.email,
       subject: "Réinitialisation de votre mot de passe",
-      html: html
-    };
-
-    return this.transport.sendMail(mailOptions);
+      html
+    });
   }
 }
