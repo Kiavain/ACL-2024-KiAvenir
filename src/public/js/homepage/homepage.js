81585e9f6381f7e641f2e2242bd5d3dab504670f
Seliaste <aena.aria@protonmail.com>
2024-12-10 19:45:32 +0100

    Fixing du backend de la preview avec la nouvelle récurrence

--- src/public/js/homepage/homepage.js
+++ src/public/js/homepage/homepage.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const previewPanel = document.getElementById("homepageDailyPreview");
+document.addEventListener('DOMContentLoaded', () => {
+  const previewPanel = document.getElementById('homepageDailyPreview');
 
   // si on est bien connecté et qu'il y a une preview
   if (previewPanel) {
-    fetch("/api/homepage/todayEvents", { method: "GET" })
+    fetch('/api/homepage/todayEvents', { method: 'GET' })
       .then((response) => response.json())
       .then((data) => {
         if (data.length > 0) {
@@ -12,9 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
               previewPanel.innerHTML += `<div><span class="fw-bold">${event.title}</span> toute la journée<br/></div>`;
             } else {
               let dateStart = new Date(event.start);
-              let start = dateStart.toLocaleString("fr-FR", { timeZone: "UTC" });
+              let start = dateStart.toLocaleString('fr-FR', { timeZone: 'UTC' });
               let dateEnd = new Date(event.end);
-              let end = dateEnd.toLocaleString("fr-FR", { timeZone: "UTC" });
+              let end = dateEnd.toLocaleString('fr-FR', { timeZone: 'UTC' });
               previewPanel.innerHTML += `<div><span class="fw-bold">${event.title}</span> du ${start} au ${end}<br/></div>`;
             }
           });
--- src/routes/homepage.js
+++ src/routes/homepage.js
@@ -1,5 +1,5 @@
-import Routeur from "../structures/Routeur.js";
-import moment from "moment/moment.js";
+import Routeur from '../structures/Routeur.js';
+import moment from 'moment/moment.js';
 
 /**
  * Les routes liées à la page d'accueil
@@ -17,7 +17,7 @@ export default class HomepageRouteur extends Routeur {
       res.render('homepage');
     });
 
-    this.router.get("/api/homepage/todayEvents", async (req, res) => {
+    this.router.get('/api/homepage/todayEvents', async (req, res) => {
       if (!res.locals.user) {
         return res.json([]);
       }
@@ -29,122 +29,64 @@ export default class HomepageRouteur extends Routeur {
 
       // Parcours chaque agendaId et récupère les événements correspondants
       const agendas = this.server.database.tables
-        .get("agendas")
+        .get('agendas')
         .filter((agenda) => agenda.verifyAgendaAccess(res.locals.user.id));
 
       for (const agenda of agendas) {
-        // Récupère les événements pour cet agenda spécifique
-        const events = agenda
-          .getEvents()
-          .filter((e) => {
-            return moment(e.startDate).isSame(new Date(), "day");
-          })
-          .map((e) => ({
-            eventId: e.eventId,
-            description: e.description,
-            title: e.name,
-            start: moment(e.startDate).format(),
-            end: moment(e.endDate).format(),
-            color: agenda.color,
-            allDay: e.allDay,
-            recurrence: e.recurrence,
-            agendaId: agenda.agendaId,
-            owner: agenda.getOwner().username,
-            canEdit: agenda.verifyCanEdit(parseInt(res.locals.user.id))
-          }));
+        // Récupère les événements non récurrents
+        const events = agenda.getEvents().filter((e) => {
+          const isToday = moment(e.startDate).isSame(new Date(), 'day');
+          return agenda.agendaId === e.agendaId && isToday && e.recurrence === 4;
+        });
+        // Récupère les occurrences d'événements récurrents
+        const recurringEvents = this.server.database.tables.get('event_occurrences').filter((o) => {
+          const isToday = moment(o.occurrenceStart).isSame(new Date(), 'day');
+          const parentEventInAgenda = agenda
+            .getEvents()
+            .map((e) => e.eventId)
+            .includes(o.eventId);
 
-        // On récupère les évènements récurrents
-        const recurringEvents = events.filter((event) => event.recurrence !== 0);
-        let adjustedRecurringEvents = [];
+          // Vérifie si l'ID de l'occurrence appartient à un événement valide
+          return isToday && parentEventInAgenda && !o.isCancelled;
+        });
 
-        // Fonction pour vérifier si une année est bissextile
-        const isLeapYear = (year) => (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+        // Mappe les événements dans le bon format
+        const formattedEvents = events.map((e) => ({
+          eventId: e.eventId,
+          description: e.description,
+          title: e.name,
+          start: moment(e.startDate).toISOString(),
+          end: moment(e.endDate).toISOString(),
+          color: agenda.color,
+          allDay: e.allDay,
+          recurrence: e.recurrence,
+          interval: 1,
+          unit: e.recurrence,
+          agendaId: agenda.agendaId,
+          canEdit: agenda.verifyCanEdit(parseInt(res.locals.user.id)),
+          owner: res.locals.user.username
+        }));
 
-        // Parcourir les jours du calendrier affiché (entre startCalendar et endCalendar)
-        let currentDate = new Date();
-        currentDate.setUTCHours(0, 0, 0, 0);
-        const endCalendar = new Date();
-        endCalendar.setUTCHours(23, 59, 59, 999);
-
-        while (currentDate < endCalendar) {
-          // Récupération des informations de la date actuelle en UTC
-          const jour = currentDate.getUTCDate();
-          const mois = currentDate.getUTCMonth() + 1; // Les mois sont de 0 à 11
-          const annee = currentDate.getUTCFullYear();
-
-          // On parcourt les évènements récurrents pour savoir si on doit en afficher un
-          for (const evt of recurringEvents) {
-            let displayEvent = false;
-            const eventStart = new Date(evt.start);
-            const eventEnd = new Date(evt.end);
-            const dureeEvent = eventEnd - eventStart; // Durée de l'événement en millisecondes
-
-            const eventStartDate = eventStart.getUTCDate();
-            const eventStartMonth = eventStart.getUTCMonth() + 1;
-            const eventStartYear = eventStart.getUTCFullYear();
-
-            // On vérifie si l'évènement doit être affiché
-            if (currentDate > eventStart) {
-              switch (evt.recurrence) {
-                case 1: // Tous les jours
-                  displayEvent = true;
-                  break;
-                case 2: // Toutes les semaines
-                  const differenceInDays = Math.floor(
-                    (currentDate.getTime() - eventStart.getTime()) / (1000 * 60 * 60 * 24)
-                  );
-                  displayEvent = differenceInDays % 7 === 0;
-                  break;
-                case 3: // Tous les mois
-                  displayEvent = eventStartDate === jour;
-                  break;
-                case 4: // Tous les ans
-                  if (eventStartMonth === mois && eventStartDate === jour) {
-                    displayEvent = true;
-                  } else if (
-                    eventStartMonth === 2 &&
-                    eventStartDate === 29 &&
-                    mois === 3 &&
-                    jour === 1 &&
-                    !isLeapYear(annee)
-                  ) {
-                    // Gestion spécifique du 29 février dans une année non bissextile
-                    displayEvent = true;
-                  }
-                  break;
-                default:
-                  break;
-              }
-            }
-
-            // Éviter d'afficher l'événement deux fois s'il tombe sur sa date de départ
-            if (eventStartDate === jour && eventStartMonth === mois && eventStartYear === annee) {
-              displayEvent = false;
-            }
-
-            if (displayEvent) {
-              // Calculons la nouvelle date de début
-              const adjustedEventStart = new Date(
-                Date.UTC(annee, mois - 1, jour, eventStart.getUTCHours(), eventStart.getUTCMinutes())
-              );
-
-              // On clone l'évènement en ajustant la date de début et de fin
-              const adjustedEvent = {
-                ...evt,
-                start: adjustedEventStart.toISOString(),
-                end: new Date(adjustedEventStart.getTime() + dureeEvent).toISOString()
-              };
-
-              adjustedRecurringEvents.push(adjustedEvent);
-            }
-          }
-
-          // Passer au jour suivant
-          currentDate.setUTCDate(currentDate.getUTCDate() + 1);
-        }
+        // Mappe les occurrences dans le bon format
+        const formattedOccurrences = recurringEvents.map((o) => ({
+          occurrenceId: o.occurrenceId,
+          eventId: o.eventId,
+          description: o.description,
+          title: o.name,
+          start: moment(o.occurrenceStart).toISOString(),
+          end: moment(o.occurrenceEnd).toISOString(),
+          color: agenda.color,
+          allDay: o.allDay,
+          isCancelled: o.isCancelled,
+          unit: o.unit,
+          interval: o.interval,
+          canEdit: agenda.verifyCanEdit(parseInt(res.locals.user.id)),
+          recurrence: 5,
+          owner: res.locals.user.username
+        }));
 
         // Ajoute les événements de cet agenda au tableau global
-        allEvents = allEvents.concat(events, adjustedRecurringEvents);
+        allEvents = allEvents.concat(formattedEvents, formattedOccurrences);
       }
 
       // Envoie tous les événements associés aux agendas spécifiés
